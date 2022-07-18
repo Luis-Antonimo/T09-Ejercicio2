b43eb61b48cc7ebdac72544a73a52f43fa48eeca
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:35:11 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace T09Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+            empleado1.ImprimeDatos();
+            empleado1.MayorEdad();
+        }
+
+        class Empleado
+        {
+            private int sueldo;
+            private string nombre;
+            private int limite;
+
+            public Empleado()
+            {
+                limite = 3000;
+                Console.WriteLine("Escriba el nombre del empleado:");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Escriba el sueldo del empleado:");
+                sueldo = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            public void ImprimeDatos()
+            {
+                Console.WriteLine("El empleado " + nombre + " tiene un sueldo de " + sueldo + "€.");
+            }
+
+            public void MayorEdad()
+            {
+                if (sueldo > limite)
+                {
+                    Console.WriteLine("El empleado " + nombre + " debe pagar impuestos.");
+                }
+                else
+                {
+                    Console.WriteLine("El empleado " + nombre + " no debe pagar impuestos.");
+                }
+            }
+        }
+    }
+}
